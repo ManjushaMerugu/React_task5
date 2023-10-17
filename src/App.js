a4614c10334bdadce37c10988fcbac86ea68e301
ManjushMerugu <merugumanjusha@gmail.com>
2023-10-17 23:47:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Age from './components/Task5';
+function App() {
+  return (
+    <div>
+    <Age/>
+    </div>
+  );
+}
+
+export default App;
